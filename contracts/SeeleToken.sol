354b01c3908b960a9eb40b686626326fe01f2ba8
reedhong <fishwarter@qq.com>
2018-02-08 23:47:35 +0800

    users claim token themselves

--- contracts/SeeleToken.sol
+++ contracts/SeeleToken.sol
@@ -24,11 +24,19 @@ contract SeeleToken is PausableToken {
     /// Fields that can be changed by functions
     mapping (address => uint) public lockedBalances;
 
+    /// claim flag
+    bool public claimedFlag;  
+
     /*
      * MODIFIERS
      */
     modifier onlyMinter {
         assert(msg.sender == minter);
+        _;
+    }
+
+    modifier canClaimed {
+        assert(claimedFlag == true);
         _;
     }
 
@@ -57,6 +65,7 @@ contract SeeleToken is PausableToken {
         {
         minter = _minter;
         totalSupply = _maxTotalSupply;
+        claimedFlag = false;
         transferOwnership(_admin);
     }
 
@@ -85,6 +94,14 @@ contract SeeleToken is PausableToken {
         return true;
     }
 
+
+    function setClaimedFlag(bool flag) 
+        public
+        onlyOwner 
+    {
+        claimedFlag = flag;
+    }
+
      /*
      * PUBLIC FUNCTIONS
      */
@@ -92,7 +109,7 @@ contract SeeleToken is PausableToken {
     /// @dev Locking period has passed - Locked tokens have turned into tradeable
     function claimTokens(address receipent)
         public
-        onlyOwner
+        canClaimed
     {
         balances[receipent] = balances[receipent].add(lockedBalances[receipent]);
         lockedBalances[receipent] = 0;
